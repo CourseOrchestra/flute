84bf718fb0eea96f567dcae77ca5adc6f3c68f7b
inponomarev <iponomarev@mail.ru>
2018-01-03 02:27:30 +0300

    Ignore OraclePollerTest (?)

--- src/test/java/ru/curs/flute/OraclePollerTest.java
+++ src/test/java/ru/curs/flute/OraclePollerTest.java
@@ -1,7 +1,9 @@
 package ru.curs.flute;
 
+import org.junit.Ignore;
 import ru.curs.flute.JDBCConnectionPool.DBType;
 
+@Ignore
 public class OraclePollerTest extends AbstractSQLTablePollerTest {
 	@Override
 	protected DBType getDBType() {
