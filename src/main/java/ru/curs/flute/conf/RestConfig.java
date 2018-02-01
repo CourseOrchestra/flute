15d0c0b3d99c8eabaac95956c58c59cde37b4b2c
ioann <ioanngolovko@gmail.com>
2018-02-01 17:37:26 +0300

    Startup bug was fixed

---
+++ src/main/java/ru/curs/flute/conf/RestConfig.java
@@ -0,0 +1,31 @@
+package ru.curs.flute.conf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import ru.curs.celesta.Celesta;
+import ru.curs.flute.GlobalParams;
+import ru.curs.flute.rest.RestMappingBuilder;
+import ru.curs.flute.source.RestTaskSource;
+import spark.Spark;
+
+@Configuration
+public class RestConfig {
+
+    public RestConfig(
+            @Autowired CommonParameters params,
+            @Autowired Celesta celesta,
+            @Autowired RestTaskSource taskSource,
+            @Autowired GlobalParams globalParams) {
+        if (params.getRestPort() == null)
+            return;
+
+
+        params.getRestPort().ifPresent(port -> {
+            Spark.port(port);
+            Spark.ipAddress(params.getRestHost());
+            RestMappingBuilder.getInstance().initRouters(celesta, taskSource, globalParams.getFluteUserId());
+        });
+
+    }
+
+}
